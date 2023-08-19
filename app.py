b93a0b24c62671c31464e1c163169ecc3bdf36df
hernantvg <hernantvg@gmail.com>
2023-08-18 23:09:12 -0300

    s

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import requests
 import datetime
+import pytz
 
 app = Flask(__name__)
 
@@ -24,14 +25,23 @@ def convert():
 
     if direction == 'usd_to_ars':
         result = amount * data['venta']
-    else:
+        currency_symbol = 'dólares'
+    elif direction == 'ars_to_usd':
         result = amount / data['venta']
+        currency_symbol = 'pesos'
+
+    formatted_amount = '{:,.2f}'.format(amount)
+    formatted_result = '{:,.2f}'.format(result)
+
 
     fecha_actualizacion = datetime.datetime.strptime(data['fechaActualizacion'], "%Y-%m-%dT%H:%M:%S.%fZ")
-    fecha_actualizacion_str = fecha_actualizacion.strftime("%d-%m-%Y %I:%M %p")
+    
+    # Convertir la fecha y hora a la zona horaria de Argentina
+    argentina_tz = pytz.timezone('America/Argentina/Buenos_Aires')
+    fecha_actualizacion = fecha_actualizacion.replace(tzinfo=pytz.utc).astimezone(argentina_tz)
+    fecha_actualizacion_str = fecha_actualizacion.strftime("%d-%m-%Y %I:%M %p %Z")
 
-    return render_template('index.html', result=result, compra=data['compra'], venta=data['venta'], fecha_actualizacion=fecha_actualizacion_str)
-
+    return render_template('index.html', formatted_amount=formatted_amount, formatted_result=formatted_result, currency_symbol=currency_symbol, compra=data['compra'], venta=data['venta'], fecha_actualizacion=fecha_actualizacion_str, direction=direction)
 
 
 if __name__ == '__main__':
