f9bc65a1a9701fd41e0576030fa7a0616ed4e5b7
hernantvg <hernantvg@gmail.com>
2023-08-19 11:24:39 -0300

    a

--- index.php
+++ index.php
@@ -15,28 +15,38 @@
                         <h2 class="card-title">Calculadora Dolar Blue a Peso Argentino</h2>
 
                         <?php
-                        require 'vendor/autoload.php';
-                        use GuzzleHttp\Client;
+                        // Ruta principal y lógica de conversión
+                        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['amount'], $_POST['direction'])) {
+                            $amount = floatval($_POST['amount']);
+                            $direction = $_POST['direction'];
 
-                        // Crear instancia de cliente HTTP
-                        $client = new Client();
+                            $response = file_get_contents("https://dolarapi.com/v1/dolares/blue");
+                            $data = json_decode($response, true);
 
-                        if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['REQUEST_URI'] === '/') {
-                            $response = $client->get("https://dolarapi.com/v1/dolares/blue");
-                            $data = json_decode($response->getBody(), true);
+                            if ($direction === 'usd_to_ars') {
+                                $result = $amount * $data['venta'];
+                                $currency_symbol = 'dólares';
+                            } elseif ($direction === 'ars_to_usd') {
+                                $result = $amount / $data['venta'];
+                                $currency_symbol = 'pesos';
+                            }
 
-                            $compra = $data['compra'];
-                            $venta = $data['venta'];
-                            $fecha_actualizacion = $data['fechaActualizacion'];
+                            $formatted_amount = number_format($amount, 2, '.', ',');
+                            $formatted_result = number_format($result, 2, '.', '');
+
+                            $fecha_actualizacion = new DateTime($data['fechaActualizacion'], new DateTimeZone('UTC'));
+                            $argentina_tz = new DateTimeZone('America/Argentina/Buenos_Aires');
+                            $fecha_actualizacion->setTimezone($argentina_tz);
+                            $fecha_actualizacion_str = $fecha_actualizacion->format('d-m-Y h:i A T');
 
                             echo '
                             <div class="alert alert-warning">
                                 <p class="mb-1"><strong>Cotización actual Dolar Blue:</strong></p>
-                                <h3><span class="text-success">Compra: <strong>'.$compra.'</strong></span> <span class="text-danger">Venta: <strong>'.$venta.'</strong></span></h3>
-                                <p class="mb-0"><span class="text-secondary">Última actualización:</span> '.$fecha_actualizacion.'</p>
+                                <h3><span class="text-success">Compra: <strong>' . $data['compra'] . '</strong></span> <span class="text-danger">Venta: <strong>' . $data['venta'] . '</strong></span></h3>
+                                <p class="mb-0"><span class="text-secondary">Última actualización:</span> ' . $fecha_actualizacion_str . '</p>
                                 <p><em>Nota: Hora de Buenos Aires, Argentina.</em></p>
                             </div>
-                            <form action="/index.php" method="post">
+                            <form action="" method="post">
                                 <div class="mb-3">
                                     <label for="amount" class="form-label">Cantidad:</label>
                                     <input type="number" step="0.01" class="form-control" name="amount" id="amount" required>
@@ -51,44 +61,22 @@
                                 <button type="submit" class="btn btn-primary">Convertir!</button>
                             </form>
                             ';
-                        }
-
-                        if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === '/index.php') {
-                            $amount = floatval($_POST['amount']);
-                            $direction = $_POST['direction'];
-
-                            $response = $client->get("https://dolarapi.com/v1/dolares/blue");
-                            $data = json_decode($response->getBody(), true);
-
-                            if ($direction === 'usd_to_ars') {
-                                $result = $amount * $data['venta'];
-                                $currency_symbol = 'dólares';
-                            } elseif ($direction === 'ars_to_usd') {
-                                $result = $amount / $data['venta'];
-                                $currency_symbol = 'pesos';
-                            }
-
-                            $formatted_amount = number_format($amount, 2, '.', ',');
-                            $formatted_result = number_format($result, 2, '.', '');
-
-                            $fecha_actualizacion = DateTime::createFromFormat('Y-m-d\TH:i:s.u\Z', $data['fechaActualizacion']);
-                            $argentina_tz = new DateTimeZone('America/Argentina/Buenos_Aires');
-                            $fecha_actualizacion->setTimezone($argentina_tz);
-                            $fecha_actualizacion_str = $fecha_actualizacion->format('d-m-Y h:i A T');
-
-                            echo '
-                            <div class="mt-4 alert alert-success">
-                                <h6>';
-                            if ($direction === 'ars_to_usd') {
+                            
+                            if (isset($formatted_result)) {
                                 echo '
-                                    Con '.str_replace(',', '.', $formatted_amount).' Pesos <img src="'.url_for('static', 'img/flags/argentina.png').'" alt="Bandera Argentina" style="width: 25px;"><br><hr><br> obtienes '.str_replace('.00', '', $formatted_result).' Dolares <img src="'.url_for('static', 'img/flags/usa.png').'" alt="Bandera Estados Unidos" style="width: 25px;">';
-                            } elseif ($direction === 'usd_to_ars') {
+                                <div class="mt-4 alert alert-success">
+                                    <h6>';
+                                if ($direction === 'ars_to_usd') {
+                                    echo '
+                                        Con ' . str_replace(',', '.', $formatted_amount) . ' Pesos <img src="' . url_for('static', 'img/flags/argentina.png') . '" alt="Bandera Argentina" style="width: 25px;"><br><hr><br> obtienes ' . str_replace('.00', '', $formatted_result) . ' Dolares <img src="' . url_for('static', 'img/flags/usa.png') . '" alt="Bandera Estados Unidos" style="width: 25px;">';
+                                } elseif ($direction === 'usd_to_ars') {
+                                    echo '
+                                        Con ' . str_replace(',', '.', $formatted_amount) . ' Dolares <img src="' . url_for('static', 'img/flags/usa.png') . '" alt="Bandera Estados Unidos" style="width: 25px;"><br><hr><br> obtienes ' . str_replace('.00', '', $formatted_result) . ' Pesos <img src="' . url_for('static', 'img/flags/argentina.png') . '" alt="Bandera Argentina" style="width: 25px;">';
+                                }
                                 echo '
-                                    Con '.str_replace(',', '.', $formatted_amount).' Dolares <img src="'.url_for('static', 'img/flags/usa.png').'" alt="Bandera Estados Unidos" style="width: 25px;"><br><hr><br> obtienes '.str_replace('.00', '', $formatted_result).' Pesos <img src="'.url_for('static', 'img/flags/argentina.png').'" alt="Bandera Argentina" style="width: 25px;">';
+                                    </h6>
+                                </div>';
                             }
-                            echo '
-                                </h6>
-                            </div>';
                         }
                         ?>
 
