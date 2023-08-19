870be26bae856bd4242a5b0814fc24ece0545bb9
hernantvg <hernantvg@gmail.com>
2023-08-19 11:44:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,53 +3,57 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cotización Dolar Blue</title>
+    <meta name="description" content="Consulta la cotización actual del Dólar Blue en Argentina y realiza conversiones de moneda.">
+    <meta name="robots" content="index, follow">
+    <title>Cotización Dólar Blue Hoy - Precio Dólar Blue Hoy</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
     <div class="container mt-5">
-        <?php
-        $api_url = "https://dolarapi.com/v1/dolares/blue";
-        $data = json_decode(file_get_contents($api_url), true);
+        <div class="card">
+            <div class="card-body">
+                <?php
+                $api_url = "https://dolarapi.com/v1/dolares/blue";
+                $data = json_decode(file_get_contents($api_url), true);
 
-        $compra = $data["compra"];
-        $venta = $data["venta"];
-        $fechaActualizacion = $data["fechaActualizacion"];
+                $compra = $data["compra"];
+                $venta = $data["venta"];
+                $fechaActualizacion = $data["fechaActualizacion"];
 
-        // Convertir la fecha a objeto DateTime
-        $fecha = new DateTime($fechaActualizacion, new DateTimeZone('UTC'));
-        // Cambiar el huso horario a GMT-3
-        $fecha->setTimezone(new DateTimeZone('America/Argentina/Buenos_Aires'));
-        $fechaFormateada = $fecha->format('Y-m-d H:i:s');
-        ?>
-        <div class="alert alert-primary" role="alert">
-            Cotización actual Dolar Blue:
-            <br>
-            Compra: $<?php echo $compra ?> Venta: $<?php echo $venta ?>
-            <br>
-            Última actualización: <?php echo $fechaFormateada ?>
-            <br>
-            Nota: Hora de Buenos Aires, Argentina (GMT-3).
-        </div>
-
-        <div class="mt-4">
-            <h4>Calculadora de Conversión</h4>
-            <form>
-                <div class="mb-3">
-                    <label for="amount" class="form-label">Cantidad</label>
-                    <input type="number" class="form-control" id="amount" placeholder="Ingrese la cantidad">
+                $fecha = new DateTime($fechaActualizacion, new DateTimeZone('UTC'));
+                $fecha->setTimezone(new DateTimeZone('America/Argentina/Buenos_Aires'));
+                $fechaFormateada = $fecha->format('Y-m-d H:i:s');
+                ?>
+                <div class="alert alert-primary" role="alert">
+                    Cotización actual Dólar Blue:
+                    <br>
+                    Compra: $<?php echo $compra ?> Venta: $<?php echo $venta ?>
+                    <br>
+                    Última actualización: <?php echo $fechaFormateada ?>
+                    <br>
+                    Nota: Hora de Buenos Aires, Argentina (GMT-3).
                 </div>
-                <div class="mb-3">
-                    <label for="conversionDirection" class="form-label">Dirección de Conversión</label>
-                    <select class="form-select" id="conversionDirection">
-                        <option value="usdToArs">USD a ARS</option>
-                        <option value="arsToUsd">ARS a USD</option>
-                    </select>
+
+                <div class="mt-4">
+                    <h4>Calculadora de Conversión</h4>
+                    <form>
+                        <div class="mb-3">
+                            <label for="amount" class="form-label">Cantidad</label>
+                            <input type="number" class="form-control" id="amount" placeholder="Ingrese la cantidad">
+                        </div>
+                        <div class="mb-3">
+                            <label for="conversionDirection" class="form-label">Dirección de Conversión</label>
+                            <select class="form-select" id="conversionDirection">
+                                <option value="usdToArs">USD a ARS</option>
+                                <option value="arsToUsd">ARS a USD</option>
+                            </select>
+                        </div>
+                        <button type="button" class="btn btn-primary" id="convert">Convertir</button>
+                    </form>
+                    <div class="mt-3">
+                        <p id="result"></p>
+                    </div>
                 </div>
-                <button type="button" class="btn btn-primary" id="convert">Convertir</button>
-            </form>
-            <div class="mt-3">
-                <p id="result"></p>
             </div>
         </div>
     </div>
