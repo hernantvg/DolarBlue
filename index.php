c455d0900626c8764dd8124cd2b545139d858439
hernantvg <111532382+hernantvg@users.noreply.github.com>
2023-08-24 15:33:06 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="description" content="Consulta la cotización actual del Dólar Blue en Argentina y realiza conversiones de moneda.">
     <meta name="keywords" content="valor,precio,cotizacion,cuanto,costo,importe,dolar,blue,paralelo,alternativo,informal,negro,mercado,venta,compra,cambio,moneda,peso,argentina,hoy,minuto,actualizado,conversor,online,convertir,calculadora,u$s,usd">
     <meta name="robots" content="index, follow">
-    <title>Calculadora de dólar blue a peso argentino - Cotización Dólar Blue Hoy - Precio Dólar Blue Hoy</title>
+    <title>USD a ARS | Calculadora de dólar blue a peso argentino - Cotización Dólar Blue Hoy - Precio Dólar Blue Hoy</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <style>
