4ca8c6f6a1fa405b9e415cfca000148af1c7ab3b
hernantvg <111532382+hernantvg@users.noreply.github.com>
2023-08-24 17:57:21 -0300

    up

--- index.php
+++ index.php
@@ -1,65 +1,72 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Consulta la cotización actual del Dólar Blue en Argentina y realiza conversiones de moneda.">
-    <meta name="keywords" content="valor,precio,cotizacion,cuanto,costo,importe,dolar,blue,paralelo,alternativo,informal,negro,mercado,venta,compra,cambio,moneda,peso,argentina,hoy,minuto,actualizado,conversor,online,convertir,calculadora,u$s,usd">
+    <meta name="description"
+        content="Consulta la cotización actual del Dólar Blue en Argentina y realiza conversiones de moneda.">
+    <meta name="keywords"
+        content="valor,precio,cotizacion,cuanto,costo,importe,dolar,blue,paralelo,alternativo,informal,negro,mercado,venta,compra,cambio,moneda,peso,argentina,hoy,minuto,actualizado,conversor,online,convertir,calculadora,u$s,usd">
     <meta name="robots" content="index, follow">
-    <title>USD a ARS | Calculadora de dólar blue a peso argentino - Cotización Dólar Blue Hoy - Precio Dólar Blue Hoy</title>
+    <title>USD a ARS | Calculadora de dólar blue a peso argentino - Cotización Dólar Blue Hoy - Precio Dólar Blue Hoy
+    </title>
     <link rel="icon" href="img/favicon.ico" type="image/x-icon">
     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <style>
-    body {
-        font-family: 'Roboto', sans-serif;
-    }
+        body {
+            font-family: 'Roboto', sans-serif;
+        }
 
-    .center-box {
-        text-align: center;
-    }
+        .center-box {
+            text-align: center;
+        }
 
-    .med-text {
-        font-size: 24px;
-        font-weight: bold;
-    }
+        .med-text {
+            font-size: 24px;
+            font-weight: bold;
+        }
 
-    .big-text {
-        font-size: 32px;
-        font-weight: bold;
-    }
-    </style>    
+        .big-text {
+            font-size: 32px;
+            font-weight: bold;
+        }
+    </style>
 </head>
+
 <body>
     <div class="container mt-5">
         <div class="card">
             <div class="card-header alert alert-secondary">
                 <h1 class="m-0">Cotización Dólar Blue Hoy</h1>
-                    <h6>USD a ARS | Calculadora de dólar blue a peso argentino</h6>
+                <h6>USD a ARS | Calculadora de dólar blue a peso argentino</h6>
             </div>
-            <div class="card-body">            
+            <div class="card-body">
                 <div class="center-box">
                     <div class="alert alert-primary" role="alert">
                         <span class="med-text">Cotización actual Dólar Blue</span><br>
                         <span class="big-text">
-                        Compra: $<span id="compra"></span>
-                        Venta: $<span id="venta"></span>
+                            Compra: $<span id="compra"></span>
+                            Venta: $<span id="venta"></span>
                         </span><br>
                         Última actualización: <span id="fechaActualizacion"></span><br>
                         Nota: Hora de Buenos Aires, Argentina (GMT-3).
                         <div role="alert" id="estadoAPI">
-                         Estado de la API: Cargando...
+                            Estado de la API: Cargando...
                         </div>
                     </div>
                 </div>
-                
+
                 <div class="card bg-light mt-4">
                     <div class="card-body">
                         <h4 class="card-title">Calculadora de Conversión Dólar Blue a Peso Argentino</h4>
                         <p>
-                        Emplea la herramienta de conversión que sigue para estimar el valor del dólar Blue o paralelo en moneda local argentina y al revés. El tipo de cambio del dólar paralelo se renueva cada día.
+                            Emplea la herramienta de conversión que sigue para estimar el valor del dólar Blue o
+                            paralelo en moneda local argentina y al revés. El tipo de cambio del dólar paralelo se
+                            renueva cada día.
                         </p>
                         <form>
                             <div class="mb-3">
@@ -74,7 +81,9 @@
                                 </select>
                             </div>
                             <div class="d-grid gap-2">
-                            <button type="button" class="btn btn-primary" id="convert"><h4>Convertir <i class="bi bi-currency-dollar"></i></h4></button>
+                                <button type="button" class="btn btn-primary" id="convert">
+                                    <h4>Convertir <i class="bi bi-currency-dollar"></i></h4>
+                                </button>
                             </div>
                         </form>
                     </div>
@@ -82,73 +91,73 @@
 
                 <!-- Muestro resultado de conversion -->
                 <div class="card-body">
-                  <p id="result"></p>
+                    <p id="result"></p>
                 </div>
 
                 <div class="card bg-light mt-4"">
 
-                    <table class="table table-striped" id="cotizaciones">
-                        <thead>
-                            <tr>
-                                <th>Cotizacion de Todos los tipos de Dólar</th>
-                                <th>Compra</th>
-                                <th>Venta</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <!-- Los datos de la API se agregarán aquí automáticamente por JavaScript -->
-                        </tbody>
+                    <table class=" table table-striped" id="cotizaciones">
+                    <thead>
+                        <tr>
+                            <th>Cotizacion de Todos los tipos de Dólar</th>
+                            <th>Compra</th>
+                            <th>Venta</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- Los datos de la API se agregarán aquí automáticamente por JavaScript -->
+                    </tbody>
                     </table>
 
                 </div>
 
                 <!-- Tabla de conversiones populares -->
                 <div class="card bg-light mt-4">
-                        <table class="table table-striped">
+                    <table class="table table-striped">
                         <thead>
                             <tr>
                                 <th>Conversiones Populares</th>
                             </tr>
                         </thead>
-                            <tbody>
-                                <tr>
-                                    <td><span id="conversion1"></span></td>
-                                </tr>
-                                <tr>
-                                    <td><span id="conversion10"></span></td>
-                                </tr>
-                                <tr>
-                                    <td><span id="conversion20"></span></td>
-                                </tr>
-                                <tr>
-                                    <td><span id="conversion50"></span></td>
-                                </tr>
-                                <tr>
-                                    <td><span id="conversion100"></span></td>
-                                </tr>
-                                <tr>
-                                    <td><span id="conversion200"></span></td>
-                                </tr>
-                                <tr>
-                                    <td><span id="conversion500"></span></td>
-                                </tr>
-                                <tr>
-                                    <td><span id="conversion1000"></span></td>
-                                </tr>
-                                <tr>
-                                    <td><span id="conversion5000"></span></td>
-                                </tr>
-                                <tr>
-                                    <td><span id="conversion10000"></span></td>
-                                </tr>
-                                <tr>
-                                    <td><span id="conversion100000"></span></td>
-                                </tr>
-                                <!-- Agrega más filas para otras conversiones populares aquí -->
-                            </tbody>
-                        </table>
+                        <tbody>
+                            <tr>
+                                <td><span id="conversion1"></span></td>
+                            </tr>
+                            <tr>
+                                <td><span id="conversion10"></span></td>
+                            </tr>
+                            <tr>
+                                <td><span id="conversion20"></span></td>
+                            </tr>
+                            <tr>
+                                <td><span id="conversion50"></span></td>
+                            </tr>
+                            <tr>
+                                <td><span id="conversion100"></span></td>
+                            </tr>
+                            <tr>
+                                <td><span id="conversion200"></span></td>
+                            </tr>
+                            <tr>
+                                <td><span id="conversion500"></span></td>
+                            </tr>
+                            <tr>
+                                <td><span id="conversion1000"></span></td>
+                            </tr>
+                            <tr>
+                                <td><span id="conversion5000"></span></td>
+                            </tr>
+                            <tr>
+                                <td><span id="conversion10000"></span></td>
+                            </tr>
+                            <tr>
+                                <td><span id="conversion100000"></span></td>
+                            </tr>
+                            <!-- Agrega más filas para otras conversiones populares aquí -->
+                        </tbody>
+                    </table>
                 </div>
-           </div>
+            </div>
         </div>
     </div>
 
@@ -164,8 +173,8 @@
                 document.getElementById("venta").textContent = venta;
 
                 const fecha = new Date(fechaActualizacion);
-                    const fechaFormateada = fecha.toLocaleString("es-AR", { timeZone: "America/Argentina/Buenos_Aires" });
-                    document.getElementById("fechaActualizacion").textContent = fechaFormateada;
+                const fechaFormateada = fecha.toLocaleString("es-AR", { timeZone: "America/Argentina/Buenos_Aires" });
+                document.getElementById("fechaActualizacion").textContent = fechaFormateada;
 
                 const conversionTable = [
                     { amount: 1, label: "1 dólar blue" },
@@ -190,7 +199,7 @@
                 console.error("Error fetching data:", error);
             });
 
-        document.getElementById("convert").addEventListener("click", function() {
+        document.getElementById("convert").addEventListener("click", function () {
             const amount = parseFloat(document.getElementById("amount").value);
             const conversionDirection = document.getElementById("conversionDirection").value;
 
@@ -212,8 +221,8 @@
             document.getElementById("result").innerHTML = result;
         });
 
-         // Función para actualizar la tabla de cotizaciones
-         function actualizarTabla() {
+        // Función para actualizar la tabla de cotizaciones
+        function actualizarTabla() {
             fetch("https://dolarapi.com/v1/dolares")
                 .then(response => response.json())
                 .then(data => {
@@ -246,17 +255,18 @@
         // Llamar a la función de actualización al cargar la página
         actualizarTabla();
 
-         // Obtener el estado de la API
-    fetch("https://dolarapi.com/v1/estado")
-        .then(response => response.json())
-        .then(data => {
-            const estado = data.estado;
-            document.getElementById("estadoAPI").textContent = `Status del servicio: ${estado}`;
-        })
-        .catch(error => {
-            console.error("Error fetching API state:", error);
-            document.getElementById("estadoAPI").textContent = "Status del servicio: Error al cargar";
-        });
+        // Obtener el estado de la API
+        fetch("https://dolarapi.com/v1/estado")
+            .then(response => response.json())
+            .then(data => {
+                const estado = data.estado;
+                document.getElementById("estadoAPI").textContent = `Status del servicio: ${estado}`;
+            })
+            .catch(error => {
+                console.error("Error fetching API state:", error);
+                document.getElementById("estadoAPI").textContent = "Status del servicio: Error al cargar";
+            });
     </script>
 </body>
+
 </html>
