f34c8e8ce5b81624092a4aac640d7ec9f36a024e
hernantvg <111532382+hernantvg@users.noreply.github.com>
2023-08-24 16:04:01 -0300

    dfsd

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta name="robots" content="index, follow">
     <title>USD a ARS | Calculadora de dólar blue a peso argentino - Cotización Dólar Blue Hoy - Precio Dólar Blue Hoy</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <style>
     body {
