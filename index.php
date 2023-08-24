85e9df4482bbac6570b9c5c03f55e1863b67951d
hernantvg <111532382+hernantvg@users.noreply.github.com>
2023-08-24 15:55:10 -0300

    ffdfs

--- index.php
+++ index.php
@@ -78,9 +78,18 @@
                 </div>
 
                 <!-- Muestro resultado de conversion -->
-                <div class="card-body">
-                  <p id="result"></p>
-                </div>
+
+                <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+                    <div class="toast-header">
+                        <img src="..." class="rounded me-2" alt="...">
+                        <strong class="me-auto">Bootstrap</strong>
+                        <small>11 mins ago</small>
+                        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                    </div>
+                    <div class="toast-body">
+                    <p id="result"></p>
+                    </div>
+                    </div>
 
                 <div class="card bg-light mt-4"">
 
