19610f588261670675f34feb34b51ee6d1fabceb
hernantvg <hernantvg@gmail.com>
2023-08-27 14:06:23 -0300

    asd

--- index2.php
+++ index2.php
@@ -247,18 +247,20 @@
                     document.getElementById("estadoAPI").textContent = "Status del servicio: Error al cargar";
                 });
 
-                if ('serviceWorker' in navigator) {
-                window.addEventListener('load', () => {
-                    navigator.serviceWorker.register('/sw.js')
-                        .then(registration => {
-                            console.log('Service Worker registrado con éxito:', registration);
-                        })
-                        .catch(error => {
-                            console.log('Fallo en el registro del Service Worker:', error);
-                        });
-                });
-            }
         </script>
+        <script>
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', () => {
+                navigator.serviceWorker.register('/sw.js')
+                    .then(registration => {
+                        console.log('Service Worker registrado con éxito:', registration);
+                    })
+                    .catch(error => {
+                        console.log('Fallo en el registro del Service Worker:', error);
+                    });
+            });
+        }
+    </script>
     </div>
 </body>
 
