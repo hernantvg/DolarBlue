509c3dd08b200d6eb5c42ac32a3de20560c29f0a
hernantvg <hernantvg@gmail.com>
2023-08-27 17:12:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 </head>
 
 <body>
-    <div class="container-md">
+    <div class="container mt-5">
             <div class="card">
                 <div class="card-header alert alert-secondary">
                     <h1 class="m-0">Cotización Dólar Blue Hoy</h1>
